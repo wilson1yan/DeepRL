d4a92c6845499c546c3a68684ecabd95eddffddd
Wilson Yan <wilson1.yan@berkeley.edu>
2019-02-03 16:38:18 -0800

    added threshold based on reward

--- aggregate_data.py
+++ aggregate_data.py
@@ -6,13 +6,17 @@ from itertools import count
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
 parser = argparse.ArgumentParser(description='Aggregate experience data')
 parser.add_argument('--game', dest='game', default='breakout', type=str)
 parser.add_argument('--start', dest='start', default=0, type=int)
 parser.add_argument('--process', dest='process', action='store_true')
+parser.add_argument('--reward_threshold', dest='reward_threshold', default=0, type=int)
 args = parser.parse_args()
 
 game, start, process = args.game, args.start, args.process
+reward_threshold = args.reward_threshold
 
 if process:
     data_folder = join('data', 'atari_data', game)
@@ -33,13 +37,44 @@ if process:
     rewards = np.concatenate(rewards, axis=0)
     dones = np.concatenate(dones, axis=0)
 
+    # Calculate episode index intervals
+    done_idxs = np.argwhere(dones).reshape(-1)
+    episode_intervals = [(0, done_idxs[0])]
+    for i in range(len(done_idxs) - 1):
+        episode_intervals.append((done_idxs[i]+1, done_idxs[i+1]))
+
+    if done_idxs[-1] != len(dones) - 1:
+        episode_intervals.append((done_idxs[-1], len(dones)-1))
+
+    episode_rewards = []
+    # Calculate episode rewards
+    for start, end in episode_intervals:
+        episode_rewards.append(np.sum(rewards[start:end]))
+
+    rewards_idx = np.argsort(episode_rewards)
+    rewards_sorted = episode_rewards[rewards_idx]
+    rewards_idx = rewards_idx[rewards_sorted > reward_threshold]
+
+    new_states, new_actions, new_rewards, new_dones = [], [], [], []
+    for idx in rewards_idx:
+        start, end = episode_intervals[idx]
+        new_states.append(states[start:end])
+        new_actions.append(actions[start:end])
+        new_rewards.append(rewards[start:end])
+        new_dones.append(dones[start:end])
+
+    new_states = np.concatenate(new_states, axis=0)
+    new_actions = np.concatenate(new_actions, axis=0)
+    new_rewards = np.concatenate(new_rewards, axis=0)
+    new_dones = np.concatenate(new_dones, axis=0)
+
     to_save_folder = join('data', 'atari_data', 'processed', game)
     if not os.path.exists(to_save_folder):
         os.mkdir(to_save_folder)
-    np.save(join(to_save_folder, f'{game}_states.npy'), states)
-    np.save(join(to_save_folder, f'{game}_actions.npy'), actions)
-    np.save(join(to_save_folder, f'{game}_rewards.npy'), rewards)
-    np.save(join(to_save_folder, f'{game}_dones.npy'), dones)
+    np.save(join(to_save_folder, f'{game}_states.npy'), new_states)
+    np.save(join(to_save_folder, f'{game}_actions.npy'), new_actions)
+    np.save(join(to_save_folder, f'{game}_rewards.npy'), new_rewards)
+    np.save(join(to_save_folder, f'{game}_dones.npy'), new_dones)
     print("Data Saved")
 else:
     to_save_folder = join('data', 'atari_data', 'processed', game)
@@ -53,20 +88,20 @@ print("Performing Reward Analysis...")
 print(f"Data Shapes: {states.shape}, {actions.shape}, {rewards.shape}, {dones.shape}")
 
 # Calculate episode index intervals
-done_idxs = np.argwhere(dones).reshape(-1)
-episode_intervals = [(0, done_idxs[0])]
-for i in range(len(done_idxs) - 1):
-    episode_intervals.append((done_idxs[i]+1, done_idxs[i+1]))
-
-if done_idxs[-1] != len(dones) - 1:
-    episode_intervals.append((done_idxs[-1], len(dones)-1))
-
-episode_rewards = []
-# Calculate episode rewards
-for start, end in episode_intervals:
-    episode_rewards.append(np.sum(rewards[start:end]))
-
-plt.figure()
-plt.hist(episode_rewards)
-plt.title(f'{game} rewards')
-plt.savefig('rewards.png')
+# done_idxs = np.argwhere(dones).reshape(-1)
+# episode_intervals = [(0, done_idxs[0])]
+# for i in range(len(done_idxs) - 1):
+#     episode_intervals.append((done_idxs[i]+1, done_idxs[i+1]))
+#
+# if done_idxs[-1] != len(dones) - 1:
+#     episode_intervals.append((done_idxs[-1], len(dones)-1))
+#
+# episode_rewards = []
+# # Calculate episode rewards
+# for start, end in episode_intervals:
+#     episode_rewards.append(np.sum(rewards[start:end]))
+#
+# plt.figure()
+# plt.hist(episode_rewards)
+# plt.title(f'{game} rewards')
+# plt.savefig('rewards.png')
