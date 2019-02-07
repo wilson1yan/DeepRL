e90f1edbf40b2f594af700443126850b6bc64370
Wilson Yan <wilson1.yan@berkeley.edu>
2019-02-06 23:43:58 -0800

    update agg data

--- aggregate_data.py
+++ aggregate_data.py
@@ -11,15 +11,21 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser(description='Aggregate experience data')
 parser.add_argument('--game', dest='game', default='breakout', type=str)
 parser.add_argument('--start', dest='start', default=0, type=int)
-parser.add_argument('--reward_threshold', dest='reward_threshold', default=0, type=int)
+parser.add_argument('--reward_threshold', dest='reward_threshold', type=int)
+parser.add_argument('--ratio', dest='ratio',  type=float)
+parser.add_argument('--mode', dest='mode', type=str)
 args = parser.parse_args()
+
+assert args.mode in ['reward', 'sample']
 
 game, start = args.game, args.start
 reward_threshold = args.reward_threshold
+ratio = args.ratio
+mode = args.mode
 
 data_folder = join('data', 'atari_data', game)
 states, actions, rewards, dones = [], [], [], []
-for i in count(start):
+for i in count(start=start):
     try:
         states.append(np.expand_dims(np.load(join(data_folder, f'{game}_states_{i}.npy')), axis=1))
         actions.append(np.load(join(data_folder, f'{game}_actions_{i}.npy')))
@@ -35,7 +41,46 @@ actions = np.concatenate(actions, axis=0)
 rewards = np.concatenate(rewards, axis=0)
 dones = np.concatenate(dones, axis=0)
 
-if reward_threshold > 0:
+if mode == 'reward':
+    if reward_threshold is None:
+        new_states = states
+        new_actions = actions
+        new_rewards = rewards
+        new_dones = dones
+    else:
+        # Calculate episode index intervals
+        done_idxs = np.argwhere(dones).reshape(-1)
+        episode_intervals = [(0, done_idxs[0])]
+        for i in range(len(done_idxs) - 1):
+            episode_intervals.append((done_idxs[i]+1, done_idxs[i+1]))
+
+        if done_idxs[-1] != len(dones) - 1:
+            episode_intervals.append((done_idxs[-1], len(dones)-1))
+
+        episode_rewards = []
+        # Calculate episode rewards
+        for start, end in episode_intervals:
+            episode_rewards.append(np.sum(rewards[start:end]))
+        episode_rewards = np.array(episode_rewards)
+
+        rewards_idx = np.argsort(episode_rewards)
+        rewards_sorted = episode_rewards[rewards_idx]
+        rewards_idx = rewards_idx[rewards_sorted > reward_threshold]
+
+        new_states, new_actions, new_rewards, new_dones = [], [], [], []
+        for idx in rewards_idx:
+            start, end = episode_intervals[idx]
+            new_states.append(states[start:end])
+            new_actions.append(actions[start:end])
+            new_rewards.append(rewards[start:end])
+            new_dones.append(dones[start:end])
+
+        new_states = np.concatenate(new_states, axis=0)
+        new_actions = np.concatenate(new_actions, axis=0)
+        new_rewards = np.concatenate(new_rewards, axis=0)
+        new_dones = np.concatenate(new_dones, axis=0)
+elif mode == 'sample':
+    assert ratio is not None and 0 < ratio <= 1
     # Calculate episode index intervals
     done_idxs = np.argwhere(dones).reshape(-1)
     episode_intervals = [(0, done_idxs[0])]
@@ -45,19 +90,13 @@ if reward_threshold > 0:
     if done_idxs[-1] != len(dones) - 1:
         episode_intervals.append((done_idxs[-1], len(dones)-1))
 
-    episode_rewards = []
-    # Calculate episode rewards
-    for start, end in episode_intervals:
-        episode_rewards.append(np.sum(rewards[start:end]))
-    episode_rewards = np.array(episode_rewards)
-
-    rewards_idx = np.argsort(episode_rewards)
-    rewards_sorted = episode_rewards[rewards_idx]
-    rewards_idx = rewards_idx[rewards_sorted > reward_threshold]
-
+    episode_intervals = np.array(episode_intervals)
+    n_episodes = len(episode_intervals)
+    indices = np.random.choice(n_episodes, size=int(n_episodes * ratio), replace=False)
+    episode_choices = episode_intervals[indices]
+    
     new_states, new_actions, new_rewards, new_dones = [], [], [], []
-    for idx in rewards_idx:
-        start, end = episode_intervals[idx]
+    for start, end in episode_choices:
         new_states.append(states[start:end])
         new_actions.append(actions[start:end])
         new_rewards.append(rewards[start:end])
@@ -67,11 +106,6 @@ if reward_threshold > 0:
     new_actions = np.concatenate(new_actions, axis=0)
     new_rewards = np.concatenate(new_rewards, axis=0)
     new_dones = np.concatenate(new_dones, axis=0)
-else:
-    new_states = states
-    new_actions = actions
-    new_rewards = rewards
-    new_dones = dones
 
 to_save_folder = join('data', 'atari_data', 'processed', game)
 if not os.path.exists(to_save_folder):
