fixed replay off by one bug

deep_rl/component/replay.py
@@ -187,7 +187,7 @@ class DummyReplay(object):
         states, _, _, dones = self.data
         frame_valid = np.zeros(len(states), dtype='int64')
         for i in range(len(states)):
-            for j in range(1, num_img_obs):
+            for j in range(1, num_img_obs+1):
                 if i - j < 0 or dones[i - j]:
                     break
             frame_valid[i] = j